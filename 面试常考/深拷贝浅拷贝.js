69c975cfead38bc5cb395827c56e6d604fb00eb9
Zhangyuchun <1456990147@qq.com>
2021-07-19 15:13:14 +0800

    '深拷贝浅拷贝知识加深'

--- 面试常考/深拷贝浅拷贝.js
+++ 面试常考/深拷贝浅拷贝.js
@@ -1,3 +1,4 @@
+//深拷贝1
 let deepCopy = (newObj,oldObj)=>{
     for(let key in oldObj){
         let item= oldObj[key];
@@ -12,5 +13,63 @@ let deepCopy = (newObj,oldObj)=>{
             newObj[key] = item;
         }
     }
-
+    return newObj;
 }
+
+//深拷贝2的写法
+let deepCopy2 = (obj)=>{
+    let copyObj = {};
+    if(obj === null) return obj;    //null也是一个空对象
+    if(obj instanceof Date) return new Date(obj);   //判断日期和正则
+    if(obj instanceof RegExp) return RegExp(obj);
+    if(typeof obj !== 'object') return obj;
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+            copyObj[key] = deepCopy2(obj[key]);
+        }
+
+    }
+    return copyObj;
+}
+
+//浅拷贝
+let shallowCopy = (oldObj)=>{
+    let newObj = {};
+    for(let i in oldObj){
+        if(oldObj.hasOwnProperty(i)){
+            newObj[i] = oldObj[i];
+        }
+
+    }
+    return newObj;
+}
+
+var obj1 = {
+    name: '你的名字',
+    age: 30,
+    hobby:{
+        '体育':'打篮球',
+        '学习':'看书',
+        '生活':'逛街'
+    }
+}
+
+var obj2 = shallowCopy(obj1);
+
+var obj3 = deepCopy({},obj1);
+var obj4 = deepCopy2(obj1)
+
+obj2.name = "变成obj2的名字";
+obj2.hobby['体育']= 'obj2打羽毛球';
+
+
+obj3.name = "变成obj3的名字";
+obj3.hobby['体育'] = 'obj3打羽毛球';
+
+obj4.name = "变成obj4的名字";
+obj4.hobby['体育'] = 'obj4打羽毛球';
+
+console.log(obj1);       //对于obj2的浅拷贝，name没有变，还是原来的"你的名字"  对象中的"体育"属性变了，变成了和obj2改的一样的   对于obj3的深拷贝，都没有变，两者互不影响
+console.log(obj2);
+console.log(obj3);
+console.log(obj4);
